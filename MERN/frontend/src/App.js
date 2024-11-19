c18d9909babc9ac5d5868523986a258a438a3b77
Parth <parthuvichare9@gmail.com>
2024-11-19 17:04:21 +0530

    Initial Commit

--- MERN/frontend/src/App.js
+++ MERN/frontend/src/App.js
@@ -27,7 +27,11 @@ const App = () => {
   // const[count,setCount]=useState(0)
   const userId=localStorage.getItem("AdminId")
 
-  const socketUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
+  const socketUrl = process.env.production.REACT_APP_BACKEND_URL || "http://localhost:8000";
+
+  // const socketUrl = io("https://backend-blogging-cgw5.onrender.com", {
+  //   withCredentials: true
+  // });
 
   // Receving Upcoming request from the server for Building WebSocket server
   useEffect(() => {
